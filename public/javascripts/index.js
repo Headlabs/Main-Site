2a0944bd6bc35a7660bb5c614bcf53d89d383213
thejourneydude <journeyofthedude@gmail.com>
2013-01-10 22:20:18 -0500

    began work on impress integration in books section

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,8 +1,16 @@
 /* Nav Bar */
 
+function element() {
+
+  var hello = 'asdf';
+  document.body(hello);
+}
+
 $(document).ready(function() {
   $('.slideshow').cycle();
+  $(".title").html( element() );
 });
+
 
 
 
