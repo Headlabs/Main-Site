d351d43d1b7a4624b94b358f673aa920e0e903d8
thejourneydude <journeyofthedude@gmail.com>
2013-01-14 18:50:34 -0500

    fixed icon issue by replacing slide.js with impress

--- public/javascripts/books.js
+++ public/javascripts/books.js
@@ -5,22 +5,3 @@
  * Time: 2:29 AM
  * To change this template use File | Settings | File Templates.
  */
-var mirage = {
-  title: "Joe",
-  quote: "I like cats"
-};
-
-
-
-var output = Mustache.render("{{title}} spends {{calc}}", mirage);
-
-
-$("document").ready(function() {
-
-  $("#impress a").click(function() {
-
-
-
-  });
-
-});
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,7 @@
 /* Nav Bar */
 
-function element() {
-
-  var hello = 'asdf';
-  document.body(hello);
-}
-
 $(document).ready(function() {
-  $('.slideshow').cycle();
-  $(".title").html( element() );
+  $('#slideshow').cycle();
 });
 
 
