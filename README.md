2de765d0769136706eab069d292f6dbf2cc389f1
Jason <jasontanner328@gmail.com>
2013-12-31 00:45:35 -0500

    init commit

--- README.md
+++
@@ -1,9 +0,0 @@
-#[My personal website](http://jasontanner.herokuapp.com/)
-
-Built using Zurb Foundation as the front-end framework
-and Sinatra for the back-end. I also used ImpressJS for the
-animation.
-
-
-##TODO
-1. Add more quotes to header.
--- main.rb
+++ main.rb
@@ -1,32 +1,6 @@
-#Route Sections
+# Route Sections
+# ==============
+
 get '/' do
   slim :index
-end
-
-get '/skills' do
-  slim :skills
-end
-
-get '/read' do
-  slim :read
-end
-  
-get '/misc' do
-  slim :misc  
-end
-
-get '/wintria' do
-  slim :wintria
-  end
-
-get '/seren' do
-  slim :seren
-  end
-
-get '/simpleng' do
-  slim :simpleng
-end
-
-get '/icons' do
-  slim :icons
 end
