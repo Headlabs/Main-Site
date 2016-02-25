0693bbcd19c36d4ffae19e6db8b91e0da9229a17
Jason Tanner <jasontanner328@gmail.com>
2016-02-24 23:31:26 -0500

    replaced css framework

--- main.rb
+++ main.rb
@@ -8,8 +8,6 @@ get '/' do
   slim :index
 end
 
-get '/get_video' do
-  content_type :json
-  
-  { video_ids: Youtube.new(params[:search_term]).query }.to_json
+get '/about-us' do
+  slim :about_us
 end
